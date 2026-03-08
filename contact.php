3d657ddfaf2f166c3b3b4410fca62976db54f0de
rdvc <ryancastaneda.dagupan@gmail.com>
2026-03-08 19:47:15 +0800

    a
    a

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// Point to PHPMailer-master since that is what is in your htdocs
+// Pointing to your specific folder structure in C:\xampp\htdocs\buildbright\
 require __DIR__ . '/PHPMailer-master/src/Exception.php';
 require __DIR__ . '/PHPMailer-master/src/PHPMailer.php';
 require __DIR__ . '/PHPMailer-master/src/SMTP.php';
@@ -13,10 +13,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         // Server settings
         $mail->isSMTP();
-        $mail->Host       = 'smtp.mail.yahoo.com'; // Rocketmail uses Yahoo SMTP
+        $mail->Host       = 'smtp.mail.yahoo.com'; 
         $mail->SMTPAuth   = true;
         $mail->Username   = 'ryan_castaneda@rocketmail.com'; 
-        $mail->Password   = 'fqfzubjpynddakvh'; // Generate an "App Password" in Yahoo settings
+        $mail->Password   = 'fqfzubjpynddakvh'; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
@@ -27,16 +27,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Content
         $mail->isHTML(true);
         $mail->Subject = "New Build Bright Inquiry: " . $_POST['interest'];
-        $mail->Body    = "<h3>New Inquiry Received</h3>
-                          <p><strong>Name:</strong> {$_POST['name']}</p>
-                          <p><strong>Email:</strong> {$_POST['email']}</p>
-                          <p><strong>Interest:</strong> {$_POST['interest']}</p>
-                          <p><strong>Message:</strong><br>{$_POST['message']}</p>";
+        
+        // Build readable email body
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $interest = htmlspecialchars($_POST['interest']);
+        $message = nl2br(htmlspecialchars($_POST['message']));
+
+        $mail->Body = "
+            <h3>New Inquiry from Build Bright Website</h3>
+            <p><strong>Name:</strong> {$name}</p>
+            <p><strong>Email:</strong> {$email}</p>
+            <p><strong>System Interest:</strong> {$interest}</p>
+            <p><strong>Message:</strong><br>{$message}</p>
+        ";
 
         $mail->send();
-        echo "<script>alert('Thank you! Your inquiry has been sent.'); window.location.href='index.html';</script>";
+        
+        // Redirect back to index with a success parameter to trigger the modal
+        header("Location: index.html?status=success#contact");
+        exit();
+
     } catch (Exception $e) {
+        // If it fails, we show the error via standard alert so you can debug
         echo "<script>alert('Message could not be sent. Mailer Error: {$mail->ErrorInfo}'); window.history.back();</script>";
     }
+} else {
+    header("Location: index.html");
+    exit();
 }
 ?>
